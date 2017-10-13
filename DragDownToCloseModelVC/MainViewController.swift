8c43fdaceacb5bee391a3d5d24e3c1e9aed480da
roger.wang[王濬淇] <roger.wang@104.com.tw>
2017-10-13 18:26:29 +0800

    first commit

---
+++ DragDownToCloseModelVC/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  DragDownToCloseModelVC
+//
+//  Created by roger on 2017/10/13.
+//  Copyright © 2017年 roger. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func goButtonPressed(_ sender: Any) {
+        self.present(ModelViewController(), animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DragDownToCloseModelVC/ModelViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ModelViewController.swift
+//  DragDownToCloseModelVC
+//
+//  Created by roger on 2017/10/13.
+//  Copyright © 2017年 roger. All rights reserved.
+//
+
+import UIKit
+
+class ModelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
